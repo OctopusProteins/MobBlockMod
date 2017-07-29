210d392905e7b13107be388101883a4998fdf64c
OctopusProteins <elijahheejoon@live.com>
2017-07-29 13:24:10 -0400

    Update 2.5.14
    WHEEEEEEE

--- src/main/java/com/MobBlockMod/MobBlockMod.java
+++ src/main/java/com/MobBlockMod/MobBlockMod.java
@@ -1,11 +1,15 @@
 package com.MobBlockMod;
 
 import com.MobBlockMod.Entity.MushroomMob.EntityMushroom;
+import com.MobBlockMod.Entity.MushroomMob.EntityMushroomMob;
 import com.MobBlockMod.Tree.WorldGenDwarfTree;
 import com.MobBlockMod.Tree.WorldGenPillarDwarfTree;
 import com.MobBlockMod.render.BlockRender;
 import com.MobBlockMod.world.MBMWorldGen;
 
+import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.init.Biomes;
+import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -13,6 +17,7 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 
@@ -22,7 +27,7 @@ public class MobBlockMod {
 //List Items and Blocks Here!
 	
     public static final String MODID = "MobBlockMod";
-    public static final String VERSION = "2.5.13";
+    public static final String VERSION = "2.5.14";
     
         // The instance of your mod that Forge uses.
     @Instance(MODID)
@@ -38,6 +43,9 @@ public class MobBlockMod {
         Registry.MyItems();    
         Registry.MyBlocks();
         Registry.registerSounds();
+        EntityMushroom.registerEntity();
+        EntityRegistry.addSpawn(EntityMushroomMob.class, 50, 1, 25, EnumCreatureType.AMBIENT, Biomes.forest, Biomes.birchForest, Biomes.birchForestHills, Biomes.coldTaiga, Biomes.coldTaigaHills, Biomes.extremeHills, Biomes.extremeHillsEdge, Biomes.extremeHillsPlus, Biomes.forestHills, Biomes.jungle, Biomes.jungleEdge, Biomes.jungleHills, Biomes.roofedForest, Biomes.savanna, Biomes.savannaPlateau, Biomes.taigaHills, Biomes.taiga);
+        EntityRegistry.addSpawn(EntityMushroomMob.class, 50, 50, 100, EnumCreatureType.AMBIENT, Biomes.mushroomIsland, BiomeGenBase.biomeRegistry.getObjectById(140));
     }
         
     @EventHandler
@@ -48,7 +56,7 @@ public class MobBlockMod {
         Registry.MyRecipes();
         if(event.getSide() == Side.CLIENT) Registry.renderItems();
         BlockRender.registerBlockRender();
-        EntityMushroom.registerEntity();
+        
     }
         
     @EventHandler // used in 1.6.2
