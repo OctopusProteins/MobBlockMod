8739f25e22d257c108852bb1182f171427557409
OctopusProteins <elijahheejoon@live.com>
2017-07-31 14:38:09 -0400

    Update 2.6.3
    Update 1.10. NO ERRORS PG!!

--- src/main/java/com/MobBlockMod/MobBlockMod.java
+++ src/main/java/com/MobBlockMod/MobBlockMod.java
@@ -27,7 +27,7 @@ public class MobBlockMod {
 //List Items and Blocks Here!
 	
     public static final String MODID = "MobBlockMod";
-    public static final String VERSION = "2.6.2";
+    public static final String VERSION = "2.6.3";
     
         // The instance of your mod that Forge uses.
     @Instance(MODID)
